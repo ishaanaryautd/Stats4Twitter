4ee2d2e0f0f0b84747baafd0ed0b7ba0f34b26df
Arya <ishaan.arya@intel.com>
2018-07-02 13:18:03 +0530

    twitter username sent

--- public/script.js
+++ public/script.js
@@ -1,17 +1,13 @@
 function testFunction(){
-	
-	//lol = JSON.stringify(lol);
 	$.ajax({
 		url: "http://localhost:8080/data" ,
 		data: {
-			"ishaan": "ish",
-			"shlok": "shl"
+			"twitterUsername": "iamsrk"
 		},
 		method: "GET",
 		dataType: "json",
 		success: function(result) {
 			console.log("success");
-			document.getElementById("afterText").innerHTML = result.ishaan + result.shlok;
 		},
 		error: function() {
 			console.log("Something went wrong, data could not be fetched");
