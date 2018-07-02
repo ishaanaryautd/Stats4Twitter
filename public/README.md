0e3ef318e2fceba765998b451461f77e9aeb1c89
Arya <ishaan.arya@intel.com>
2018-07-02 13:00:33 +0530

    text on html

--- public/README.md
+++
@@ -1,1 +0,0 @@
-# TwitterStats
--- public/script.js
+++ public/script.js
@@ -4,13 +4,14 @@ function testFunction(){
 	$.ajax({
 		url: "http://localhost:8080/data" ,
 		data: {
-			"ishaan": "1",
-			"shlok": "2"
+			"ishaan": "ish",
+			"shlok": "shl"
 		},
 		method: "GET",
 		dataType: "json",
 		success: function(result) {
 			console.log("success");
+			document.getElementById("afterText").innerHTML = result.ishaan + result.shlok;
 		},
 		error: function() {
 			console.log("Something went wrong, data could not be fetched");
