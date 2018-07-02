0dbe4f378a5c8283f3d0c56331679b971ce50d40
Shlok Kothari <kotharims@gmail.com>
2018-07-02 15:43:09 +0400

    added code to get tweets

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'qpb9DuJtjXe29MexkJiyF01Wg',
-    consumer_secret: 'dIokPh4NHmZZfTkBQqChKYSTg6ujRDhtmA2fAb5woMiChUdTC8',
-    access_token_key: 'fiIFiG7uiR5kFmyO7s1w9ysazYPvz1F8STCIUkx',
-    access_token_secret: '3mZIRnjblLvPF0HoHwKxfPUxzyrxtRS7xcWIp9YMhlzdh'
-};
