bf599787872c24b881b74000ee2b0f85bbe8e551
Arya <ishaan.arya@intel.com>
2018-07-04 14:28:59 +0530

    showing fetched values on web page

--- public/script.js
+++ public/script.js
@@ -8,6 +8,16 @@ function fetchData(){
 		dataType: "json",
 		success: function(result) {
 			console.log(result);
+			document.getElementById("like1").innerHTML = result.Top5LikedTweets[4].tweet;
+			document.getElementById("like2").innerHTML = result.Top5LikedTweets[3].tweet;
+			document.getElementById("like3").innerHTML = result.Top5LikedTweets[2].tweet;
+			document.getElementById("like4").innerHTML = result.Top5LikedTweets[1].tweet;
+			document.getElementById("like5").innerHTML = result.Top5LikedTweets[0].tweet;
+			document.getElementById("retweet1").innerHTML = result.Top5RetweetedTweets[4].tweet;
+			document.getElementById("retweet2").innerHTML = result.Top5RetweetedTweets[3].tweet;
+			document.getElementById("retweet3").innerHTML = result.Top5RetweetedTweets[2].tweet;
+			document.getElementById("retweet4").innerHTML = result.Top5RetweetedTweets[1].tweet;
+			document.getElementById("retweet5").innerHTML = result.Top5RetweetedTweets[0].tweet; 
 		},
 		error: function() {
 			console.log("Something went wrong, data could not be fetched");
